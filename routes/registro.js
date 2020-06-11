763c1c4f7f25d5b0ae85622688ac3794d41ee9e6
andrewzhbj <42502197+andrewzhbj@users.noreply.github.com>
2020-06-11 10:46:37 -0300

    registro
    verificacion de nuevo usuario

--- routes/registro.js
+++ routes/registro.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var cuentas = require("../public/accounts.json");
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send({
+    status : true,
+    response : cuentas
+  });
 });
+
+router.post('/', function(req, res, next) {
+
+  cuentas.push(req.body);
+
+  res.send({
+    status : true,
+    response : cuentas
+  });
+});
+
 
 module.exports = router;
