03b2459c995de25ccd65c24afce4c8850a379a98
andrewzhbj <42502197+andrewzhbj@users.noreply.github.com>
2020-06-13 22:21:41 -0300

    Se cambio la solapa de Juegos en Resenas

--- routes/registro.js
+++ routes/registro.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var cuentas = require("../public/data/accounts.json");
-
 router.get('/', function(req, res, next) {
   res.send({
     status : true,
